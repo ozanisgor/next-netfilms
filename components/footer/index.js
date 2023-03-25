eb7a654c007127f5ecafa56738c236d93863a2a6
ozan isgor <ozanisgor11@gmail.com>
2023-03-25 16:02:21 +0300

    ADD: footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,18 @@
+import Link from 'next/link'
+
+import styles from './styles.module.css'
+
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer className={styles.footer}>
+      Made with ❤️ by&nbsp;
+      <Link
+        href="https://github.com/ozanisgor"
+        target="_blank"
+      >
+        Ozan Isgor
+      </Link>
+    </footer>
+  )
 }
 export default Footer
